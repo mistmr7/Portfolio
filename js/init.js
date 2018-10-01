75125d3c0ffc53002d657fe0d67a6a9004cd7434
mistmr7 <michael.mistarz@gmail.com>
2018-09-30 21:03:50 -0400

    commit working base

---
+++ js/init.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
