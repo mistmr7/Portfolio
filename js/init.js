a81bcada5c6adc427b553bf09c25f3ca77a435f2
mistmr7 <michael.mistarz@gmail.com>
2018-10-01 14:28:32 -0400

    fixed # errors

--- js/init.js
+++ js/init.js
@@ -52,5 +52,17 @@ $(document).ready(function (){
     indicators: true
   });
   $('.modal').modal();
+  $('li a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
 })
 
